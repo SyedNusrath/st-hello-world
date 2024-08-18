05183d4cdedd7f8ee27e6696c5882f477d1418e9
SyedNusrath <syednusrath323@gmail.com>
2024-08-18 23:36:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ st.write('LTT <3')
 
 
 x = st.slider("Select your LTT value")
-st.write("your selected value :", x, "LTT value :", x * x)
+st.write("your selected value :", x, "mine LTT value :", x * x)
